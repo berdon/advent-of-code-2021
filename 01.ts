adc7eb320bc64f18f395df60d906a81c933ea7e4
Austin <berdon@gmail.com>
2021-12-01 15:41:39 -0600

    Initial commit and Day 01 solution

---
+++ 01.ts
@@ -0,0 +1,51 @@
+const got = require('got');
+
+const DEBUG = false;
+const URI_INPUT = "https://adventofcode.com/2021/day/1/input";
+const AOC_AUTHN_COOKIES = "";
+const SAMPLE_DATA = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+(async() => {
+    // ### Part 1
+    const depthReadings = await getInputDataAsync()
+    const increasedReadings = depthReadings
+        .map((value, index, values) => { return {
+            previous: index > 0 ? values[index - 1] : null,
+            current: value
+        }})
+        .map(pair => pair.previous != null && pair.previous < pair.current ? { value: pair.current, increased: true } : { value: pair.current, increased: false })
+    const increases = increasedReadings.reduce((previous, current) => previous + (current.increased ? 1 : 0), 0)
+    console.log(`Part 1: ${increases} increases observed of ${depthReadings.length} depth values`)
+
+    // ### Part 2
+    const windowedDepths = depthReadings
+        .map((value, index, values) => {return {
+            current: value,
+            window: getSumWindow(index, 3, values)
+        }})
+        .filter(pair => pair.window != null)
+        .map((value, index, values) => { return {
+            previous: index > 0 ? values[index - 1].window : null,
+            current: value.window!
+        }})
+    const windowedReadings = windowedDepths
+        .map(pair => pair.previous != null && pair.previous < pair.current ? { value: pair.current, increased: true } : { value: pair.current, increased: false })
+    const windowedIncreases = windowedReadings.reduce((previous, current) => previous + (current.increased ? 1 : 0), 0)
+    console.log(`Part 2: ${windowedIncreases} increases observed of ${depthReadings.length} depth values`)
+})();
+
+async function getInputDataAsync(): Promise<number[]> {
+    if (DEBUG) return SAMPLE_DATA;
+    const response = await got(URI_INPUT, { headers: { Cookie: AOC_AUTHN_COOKIES } })
+    const depthReadings: string[] = response.body.split('\n')
+    return depthReadings.map(value => parseInt(value))
+}
+
+function getSumWindow(index: number, size: number, values: number[]): number|null {
+    if (index < (size - 1)) return null;
+    var sum = 0
+    for (var i = Math.max(0, index - (size - 1)); i <= index; i++) {
+        sum += values[i]
+    }
+    return sum
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# advent-of-code-2021
-Advent of Code 2021
+# Advent of Code 2021
+
+## Getting Started
+
+1. Open the root directory in VS Code
+2. Open a problem typescript file (eg. `./01.ts`)
+3. Replace the value of `AOC_AUTHN_COOKIES` with your session cookie value (see below)
+4. Press `F5` to see the output
+
+## How to get your advent of code session cookie value
+
+Load [advent of code](https://adventofcode.com) with the devloper console open on the network tab. Inspect the GET request for `adventofcode.com` and find the request header for `Cookie`. The value should look something like: `session=53616c7465645f5fd59b2b6650b020709861e627asdfasd7493f145d4993fcaa81c1aa791b6f6e5f706352d6a26b0`. Copy this value as the string value for `AOC_AUTHN_COOKIES`.
