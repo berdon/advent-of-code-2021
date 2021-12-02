e3a7dd75d5340ffbf1a4207acd87b1d3510bcb32
Austin <berdon@gmail.com>
2021-12-02 09:12:36 -0600

    Day 2, got typeings, refactoring

---
+++ 02.ts
@@ -0,0 +1,78 @@
+import AOC_AUTHN_COOKIES from './authtoken.json';
+import * as got from "got"
+
+namespace Problem02 {
+    const DEBUG = false
+    const URI_INPUT = "https://adventofcode.com/2021/day/2/input"
+    const URI_SUBMIT = "https://adventofcode.com/2021/day/2/answer"
+    const SAMPLE_DATA = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2",
+    ];
+
+    (async() => {
+        // ### Part 1
+        const data = await getInputDataAsync()
+        var horizontalPosition = 0
+        var depth = 0
+        data.forEach(command => {
+            switch (command.direction) {
+                case "forward": horizontalPosition += command.amount; break
+                case "down": depth += command.amount; break
+                case "up": depth -= command.amount; break
+            }
+        })
+        console.log(`Part 1: Product of position and depth is ${horizontalPosition * depth} (${horizontalPosition} x ${depth})`)
+
+        // ### Part 2
+        var horizontalPosition = 0
+        var depth = 0
+        var aim = 0
+        data.forEach(command => {
+            switch (command.direction) {
+                case "forward":
+                    horizontalPosition += command.amount
+                    depth += aim * command.amount
+                    break
+                case "down": aim += command.amount; break
+                case "up": aim -= command.amount; break
+            }
+        })
+        console.log(`Part 2: Product of position and depth is ${horizontalPosition * depth} (${horizontalPosition} x ${depth})`)
+    })();
+
+    async function submitAnswerAsync(value: string, part: string) {
+        try {
+            const response = await got.post(URI_SUBMIT, {
+                json: { level: part, answer: value } as any,
+                headers: { Cookie: AOC_AUTHN_COOKIES }
+            })
+            console.log(response.statusCode);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function getInputDataAsync(): Promise<{ direction: "forward"|"down"|"up", amount: number }[]> {
+        var lines: string[]
+        if (DEBUG) {
+            lines = SAMPLE_DATA
+        }
+        else {
+            const response = await got.default(URI_INPUT, { headers: { Cookie: AOC_AUTHN_COOKIES } })
+            lines = response.body.split('\n')
+        }
+
+        
+        const data = lines.map(line => {
+            const tokens = line.split(" ")
+            return { direction: tokens[0] as "forward"|"down"|"up", amount: parseInt(tokens[1]) }
+        })
+
+        return data
+    }
+}
